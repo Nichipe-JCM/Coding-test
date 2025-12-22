b9770678e435d8c0ad95486fa0b87d370bfb25d8
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2025-12-22 09:25:48 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    string temp = to_string(a) + to_string(b);
+    if (stoi(temp) > 2 * a * b){
+        answer = stoi(temp);
+    } else answer = 2 * a * b;
+    return answer;
+}
