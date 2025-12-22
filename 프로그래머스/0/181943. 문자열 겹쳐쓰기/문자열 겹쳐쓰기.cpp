059c72fd25d29958da051283cc5966391d58fede
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2025-12-22 09:32:53 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    for(int i=0;i<my_string.length();i++){
+        if(i>=s&&i<overwrite_string.length()+s){
+            answer+=overwrite_string[i-s];
+        } else{
+            answer+=my_string[i];
+        }
+    }
+    return answer;
+}
