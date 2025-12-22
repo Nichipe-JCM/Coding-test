0fbf3512a7e1305cad1c3a9cdca0ac247b6c363b
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2025-12-22 09:27:11 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.04 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    answer.push_back(n);
+    for(;;){
+        if(n%2==0){
+            n /= 2;
+            answer.push_back(n);
+        } else {
+            n = 3*n+1;
+            answer.push_back(n);
+        }
+        if(n == 1){
+            break;
+        }
+    }
+    return answer;
+}
