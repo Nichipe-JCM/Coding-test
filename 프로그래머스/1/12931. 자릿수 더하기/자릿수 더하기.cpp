779ca599cc883cc9d2236dbb65efac514c7516a9
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2025-12-23 09:40:02 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    string str = to_string(n);
+    for(int i = 0; i < str.length(); i++){
+        answer += str[i] - '0';
+    }
+    return answer;
+}
