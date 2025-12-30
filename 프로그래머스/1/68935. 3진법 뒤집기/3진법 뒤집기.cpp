298fc658cf1d50e851b1ad24300a73903dfe148c
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2025-12-30 09:37:35 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 3.76 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    string str = "";
+    while (n > 1) {
+        str += to_string(n % 3);
+        n /= 3;
+    }
+    if (n == 1) str += '1';
+    reverse(str.begin(), str.end());
+    int pow = 1;
+    for(int i = 0; i < str.length(); i++){
+        answer += (str[i] - '0') * pow;
+        pow *= 3;
+    }
+    return answer;
+}
