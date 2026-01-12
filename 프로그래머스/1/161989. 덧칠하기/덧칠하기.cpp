76d83567549c286f6eb8ec484708c3e416a5ba2d
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-01-12 09:44:24 +0900

    [level 1] Title: 덧칠하기, Time: 0.24 ms, Memory: 5.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 0;
+    int painted = 0;
+    for(int s : section){
+        if(painted < s){
+            answer++;
+            painted = s + m - 1;
+        }
+    }
+    return answer;
+}
