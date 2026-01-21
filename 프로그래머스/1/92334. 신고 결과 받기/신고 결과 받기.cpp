6984e17b50aeb1df7d2b1e7878cf6722587a1bd4
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-01-21 10:05:11 +0900

    [level 1] Title: 신고 결과 받기, Time: 675.84 ms, Memory: 35.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    vector<int> answer(id_list.size(),0);
+    map<string, int> repoCount;
+    map<string, vector<string>> repoID;
+    unordered_set<string> ban;
+    
+    for(string s : report){
+        stringstream ss(s);
+        string reporter;
+        string reported;
+        ss >> reporter >> reported;
+        auto it = find(repoID[reporter].begin(), repoID[reporter].end(), reported);
+        if (it != repoID[reporter].end()) continue;
+        repoID[reporter].push_back(reported);
+        repoCount[reported]++;
+        if(repoCount[reported] >= k) ban.insert(reported);
+    }
+    
+    for(int i = 0; i < id_list.size(); i++){
+        for(string b : ban){
+            auto it = find(repoID[id_list[i]].begin(), repoID[id_list[i]].end(), b);
+            if (it != repoID[id_list[i]].end()) answer[i]++;
+        }
+    }
+        
+    return answer;
+}
