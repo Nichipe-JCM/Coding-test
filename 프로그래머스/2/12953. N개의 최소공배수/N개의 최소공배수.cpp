7389b86ec5362afbcecebb24bad9fc473fd305c7
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-01-26 09:53:57 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -1,15 +1,28 @@
 #include <string>
 #include <vector>
-#include <numeric>
 
 using namespace std;
 
 int solution(vector<int> arr) {
     int answer = 0;
     int a = arr[0];
+    int gcd;
     for(int i = 1; i < arr.size(); i++){
         int b = arr[i];
-        a = lcm(a,b);
+        int tempa = a;
+        int tempb = b;
+        while(true){
+            int r = tempa % tempb;
+            if(r == 0){
+                gcd = tempb;
+                break;
+            }
+            else{
+                tempa = tempb;
+                tempb = r;
+            }
+        }
+        a = (a / gcd) * b;
     }
     answer = a;
     return answer;
