ff232ffe97c678103aaf785b8cd5d27bec53c099
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-01-27 09:30:09 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 3.74 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    long long answer = 0;
+    vector<long long> vec(n,0);
+    vec[0] = 1;
+    vec[1] = 2;
+    for(int i = 2; i < n; i++){
+        vec[i] = (vec[i-2] + vec[i-1]) % 1234567;
+    }
+    answer = vec[n-1];
+    return answer;
+}
