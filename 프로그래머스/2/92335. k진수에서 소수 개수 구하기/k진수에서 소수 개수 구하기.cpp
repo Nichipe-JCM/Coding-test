ce31a9a32e0f937dbb0905f670f33f2881f07bca
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-02-04 09:46:19 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 8.45 ms, Memory: 4.27 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+bool isPrime(long long n)
+{
+    if (n == 1) return false;
+    for (long long i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int solution(int n, int k) {
+    int answer = 0;
+    string kbase = "";
+    string temp = "";
+    
+    while(n > 0)
+    {
+        int temp = n % k;
+        kbase += to_string(temp);
+        n /= k;
+    }
+    
+    reverse(kbase.begin(), kbase.end());
+    
+    for (char c : kbase)
+    {
+        if (c == '0')
+        {
+            if (temp == "" || temp == "1"){
+                temp = "";
+                continue;
+            }
+            long long tempn = stoll(temp);
+            if (isPrime(tempn)) answer++;
+            temp = "";
+            continue;
+        }
+        temp += c;
+    }
+    if (temp != "")
+    {
+        long long tempn = stoll(temp);
+        if (isPrime(tempn)) answer++;
+    }
+    return answer;
+}
