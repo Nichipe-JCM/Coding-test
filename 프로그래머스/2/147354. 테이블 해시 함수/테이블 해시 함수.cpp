39d204eb69aa1abf494969024e387f920e9f010d
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-02-27 09:33:58 +0900

    [level 2] Title: 테이블 해시 함수, Time: 4.86 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -12,15 +12,14 @@ int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
         return a[0] > b[0];
     });
     
-    vector<int> S_i(data.size(), 0);
-    
     for (int i = row_begin - 1; i <= row_end - 1; i++)
     {
+        int S_i = 0;
         for (const int& j : data[i])
         {
-            S_i[i] += j % (i + 1);
+            S_i += j % (i + 1);
         }
-        answer ^= S_i[i];
+        answer ^= S_i;
     }
     
     return answer;
