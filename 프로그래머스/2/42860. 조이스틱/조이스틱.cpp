f24f4d4a2ffbcd7b5793f563fbb595d2cbce998e
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-03-09 09:47:36 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int alphabet(const char& c)
+{
+    int num = c - 'A';
+    return min(num, 26 - num);
+}
+
+int solution(string name) {
+    int answer = 0;
+    int length = name.length();
+    int route1 = length - 1;
+    
+    for (int i = 0; i < length; i++)
+    {
+        answer += alphabet(name[i]);
+        int nexti = i + 1;
+        if (nexti < length)
+        {
+            while (nexti < length && name[nexti] == 'A')
+            {
+                nexti++;
+            }
+        }
+        
+        int route2 = i + i + length - nexti;
+        int route3 = (length - nexti) * 2 + i;
+        int temp = min(route2, route3);
+        route1 = min(route1, temp);
+    }
+    answer += route1;
+    return answer;
+}
