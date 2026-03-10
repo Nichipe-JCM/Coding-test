113198df97f216bbdc0c4ddb4a56cd601c3dbd80
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-03-10 10:15:42 +0900

    [level 2] Title: 땅따먹기, Time: 7.47 ms, Memory: 26.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int> > land)
+{
+    int answer = 0;
+    
+    for(int i = 1; i < land.size(); i++)
+    {
+        for(int j = 0; j < land[i].size(); j++)
+        {
+            int prevmax = -1;
+            for(int k = 0; k < land[i - 1].size(); k++)
+            {
+                if (k == j) continue;
+                prevmax = max(prevmax, land[i - 1][k]);
+            }
+            land[i][j] = land[i][j] + prevmax;
+        }
+    }
+    
+    answer = *max_element(land[land.size() - 1].begin(), land[land.size() - 1].end());
+    return answer;
+}
