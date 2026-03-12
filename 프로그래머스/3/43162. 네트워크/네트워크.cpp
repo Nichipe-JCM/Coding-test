28c489f14c382e1464ba592f4cea3bd842f80b4a
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-03-12 09:38:17 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -4,6 +4,23 @@
 
 using namespace std;
 
+void BFS(const int& n, const vector<vector<int>>& computers, queue<int>& q, vector<bool>& isvisited)
+{
+    while(!q.empty())
+    {
+        int current = q.front();
+        q.pop();
+        for(int i = 0; i < n; i++)
+        {
+            if (computers[current][i] == 1 && !isvisited[i])
+            {
+                q.push(i);
+                isvisited[i] = true;
+            }
+        }
+    }
+}
+
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
     queue<int> q;
@@ -16,19 +33,7 @@ int solution(int n, vector<vector<int>> computers) {
             answer++;
             q.push(i);
             isvisited[i] = true;
-            while(!q.empty())
-            {
-                int current = q.front();
-                q.pop();
-                for(int j = 0; j < n; j++)
-                {
-                    if (computers[current][j] == 1 && !isvisited[j])
-                    {
-                        q.push(j);
-                        isvisited[j] = true;
-                    }
-                }
-            }
+            BFS(n, computers, q, isvisited);
         }
     }
     return answer;
