5959346df0cd47ce4b4846dee8170b3a8f7ef7c8
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-03-12 10:17:26 +0900

    [level 2] Title: 구명보트, Time: 1.81 ms, Memory: 4.59 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    int left = 0, right = people.size() - 1;
+    sort(people.begin(), people.end());
+    while(left < right)
+    {
+        if (people[left] + people[right] <= limit)
+        {
+            left++;
+            right--;
+            answer++;
+        }
+        else
+        {
+            right--;
+            answer++;
+        }
+    }
+    if (left == right) answer++;
+    return answer;
+}
