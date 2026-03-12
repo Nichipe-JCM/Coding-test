f12d7a35180bee02b9b2a3d86637d2b628cdfadc
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-03-12 09:28:35 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    queue<int> q;
+    vector<bool> isvisited(n);
+    
+    for(int i = 0; i < n; i++)
+    {
+        if (!isvisited[i])
+        {
+            answer++;
+            q.push(i);
+            while(!q.empty())
+            {
+                int current = q.front();
+                q.pop();
+                for(int j = 0; j < n; j++)
+                {
+                    if (computers[current][j] == 1 && !isvisited[j])
+                    {
+                        q.push(j);
+                        isvisited[j] = true;
+                    }
+                }
+            }
+        }
+    }
+    return answer;
+}
