7d92bda69bde1f732e4659eead2178b2f92a4d62
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-03-23 09:36:25 +0900

    [level 2] Title: [3차] n진수 게임, Time: 7.99 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string tobase(int num, int n){
+    if (num == 0) return "0";
+    
+    string result = "";
+    string numbers = "0123456789ABCDEF";
+    
+    while (num > 0)
+    {
+        result += numbers[num % n];
+        num /= n;
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+string solution(int n, int t, int m, int p) {
+    string answer = "";
+    string fullnumbers = "";
+    int totalnumbercount = t * m;
+    for(int i = 0; i < totalnumbercount; i++)
+    {
+        fullnumbers += tobase(i, n);
+    }
+    for(int i = p - 1; i < fullnumbers.size(); i += m)
+    {
+        answer += fullnumbers[i];
+        if (answer.size() == t) break;
+    }
+    return answer;
+}
