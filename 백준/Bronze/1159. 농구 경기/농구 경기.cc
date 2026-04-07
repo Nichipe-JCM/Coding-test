873e69a87883739e2ca69e2278d610464055992d
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-04-07 09:36:04 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    
+    map<char, int> namemap;
+    for(int i = 0; i < N; i++)
+    {
+        string s;
+        cin >> s;
+        namemap[s[0]]++;
+    }
+    string over5list = "";
+    for(const auto& pair : namemap)
+    {
+        if (pair.second >= 5) over5list += pair.first;
+    }
+    if (over5list == "") cout << "PREDAJA";
+    else cout << over5list;
+}
