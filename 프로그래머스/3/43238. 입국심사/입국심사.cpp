9acf277377794c5f6a08db7c8065301a2dfa363a
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-04-20 09:43:41 +0900

    [level 3] Title: 입국심사, Time: 46.80 ms, Memory: 6.73 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    auto minmax = minmax_element(times.begin(), times.end());
+    long long mintime = *minmax.first;
+    long long maxtime = (long long)*minmax.second * n;
+    while(mintime <= maxtime)
+    {
+        long long median = (mintime + maxtime) / 2;
+        long long passed = 0;
+        for(int i : times) passed += median / i;
+        if (passed >= n)
+        {
+            answer = median;
+            maxtime = median - 1;
+        }
+        else mintime = median + 1;
+    }
+    return answer;
+}
