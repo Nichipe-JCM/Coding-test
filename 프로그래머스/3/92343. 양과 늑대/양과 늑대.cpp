83745428bdc2425dad0e6f3e263daa7bd80a732c
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-05-07 10:03:03 +0900

    [level 3] Title: 양과 늑대, Time: 16.15 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -2,28 +2,45 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
-void backtracking(const vector<int>& info, unordered_map<int, vector<int>>& tree, int& answer, int wolf, int sheep, int node, vector<int> candidates){
+set<vector<int>> seen;
+
+void backtracking(const vector<int>& info, unordered_map<int, vector<int>>& tree, int& answer, int wolf, int sheep, int node, vector<int> candidates, vector<int> visited){
     int cursheep = info[node] == 0 ? sheep + 1 : sheep;
     int curwolf = info[node] == 1 ? wolf + 1 : wolf;
     if (curwolf >= cursheep) return;
+    
     answer = cursheep > answer ? cursheep : answer;
     
+    visited[node] = true;
+    if(seen.count(visited)) return;
+    seen.insert(visited);
+    
     candidates.erase(remove(candidates.begin(), candidates.end(), node), candidates.end());
-    for(int i : tree[node]) candidates.push_back(i);
+    for(int i : tree[node])
+    {
+        candidates.push_back(i);
+    }
+    
     for(int i : candidates)
     {
-        backtracking(info, tree, answer, curwolf, cursheep, i, candidates);
+        backtracking(info, tree, answer, curwolf, cursheep, i, candidates, visited);
     }
 }
 
 int solution(vector<int> info, vector<vector<int>> edges) {
     int answer = 0;
     unordered_map<int, vector<int>> tree;
-    for(const vector<int>& v : edges) tree[v[0]].push_back(v[1]);
-    backtracking(info, tree, answer, 0, 0, 0, {});
+    for(const vector<int>& v : edges)
+    {
+        tree[v[0]].push_back(v[1]);
+    }
+    vector<int> visited(info.size(), 0);
+    
+    backtracking(info, tree, answer, 0, 0, 0, {}, visited);
     
     return answer;
 }
