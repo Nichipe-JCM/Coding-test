cb6c4be2935c33811fc3107e34a1d87c89694025
Nichipe <79696026+Nichipe-JCM@users.noreply.github.com>
2026-05-12 09:37:42 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 3.99 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int findparent(int i, vector<int>& parent){
+    if (parent[i] == i) return i;
+    return parent[i] = findparent(parent[i], parent);
+}
+
+void setunion(int a, int b, vector<int>& parent){
+    int aparent = findparent(a, parent);
+    int bparent = findparent(b, parent);
+    
+    if (aparent < bparent) parent[bparent] = aparent;
+    else if(aparent > bparent) parent[aparent] = bparent;
+}
+
+int solution(int n, vector<vector<int>> costs) {
+    int answer = 0;
+    vector<int> parent(100, 0);
+    for(int i = 0; i < 100; i++) parent[i] = i;
+    
+    sort(costs.begin(), costs.end(), [](const vector<int>& a, const vector<int>& b){
+        return a[2] < b[2];
+    });
+    
+    for(const vector<int>& vec : costs)
+    {
+        if (findparent(vec[0], parent) == findparent(vec[1], parent)) continue;
+        
+        setunion(vec[0], vec[1], parent);
+        answer += vec[2];
+    }
+    
+    return answer;
+}
